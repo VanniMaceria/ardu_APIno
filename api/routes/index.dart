751d7101fe7748642011e464d3567227373ecd17
Andrea Bucci <andreabucci123@gmail.com>
2025-09-25 15:09:46 +0200

    Add comment for potential deployment link

--- api/routes/index.dart
+++ api/routes/index.dart
@@ -1,5 +1,7 @@
 import 'package:dart_frog/dart_frog.dart';
 
+//eventualmente per un deploy posso usare questo -> https://dart-frog.dev/deploy/globe/ 
+
 Response onRequest(RequestContext context) {
   return Response(body: 'Welcome to Dart Frog!');
 }
